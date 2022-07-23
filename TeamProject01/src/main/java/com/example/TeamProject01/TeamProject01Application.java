b61dbbc8530b210d3d5941deec7baf98b3ef52f9
Hannie Shin <50253860+hannie0615@users.noreply.github.com>
2022-07-23 09:34:44 +0900

    Add files via upload

---
+++ TeamProject01/src/main/java/com/example/TeamProject01/TeamProject01Application.java
@@ -0,0 +1,13 @@
+package com.example.TeamProject01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamProject01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeamProject01Application.class, args);
+	}
+
+}
---
+++ TeamProject01/src/test/java/com/example/TeamProject01/TeamProject01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.TeamProject01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeamProject01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
